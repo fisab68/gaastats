46829e33c5e4f965b7258d4c9f630c82ef1f47f5
fisab68 <74133478+fisab68@users.noreply.github.com>
2026-03-04 17:56:17 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// GAA Stats PWA — Service Worker v8
+// GAA Stats PWA — Service Worker v12
 // Bump CACHE_NAME any time you update the app to force clients to reload
-const CACHE_NAME = 'gaa-stats-v11';
+const CACHE_NAME = 'gaa-stats-v13';
 
 const PRECACHE = [
   './index.html'
